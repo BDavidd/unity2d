2ec5dbc60e3073a470db571a1a3601a0d963d4a8
David Barzi <barzi.david@gmail.com>
2019-01-18 14:45:44 +0200

    Completed Section 2, Lecture 10

--- NumberWizardConsole/Number Wizard Console/Assets/NumberWizard.cs
+++ NumberWizardConsole/Number Wizard Console/Assets/NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int maximum = 1000;
+        int minimum = 1;
+        
         Debug.Log("Welcome to number wizard!");
         Debug.Log("Pick a number");
-        Debug.Log("The highest number is: 1000");
-        Debug.Log("The lowest number is: 1");
+        Debug.Log("The highest number is: " + maximum);
+        Debug.Log("The lowest number is: " + minimum);
     }
 
     // Update is called once per frame
