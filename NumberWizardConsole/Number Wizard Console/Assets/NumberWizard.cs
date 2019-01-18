b57bc6eabe6977c41bdbde16ef850640bc3aeaaa
David Barzi <barzi.david@gmail.com>
2019-01-18 16:38:03 +0200

    Completed Section 2 Lecture 11

--- NumberWizardConsole/Number Wizard Console/Assets/NumberWizard.cs
+++ NumberWizardConsole/Number Wizard Console/Assets/NumberWizard.cs
@@ -14,11 +14,25 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Pick a number");
         Debug.Log("The highest number is: " + maximum);
         Debug.Log("The lowest number is: " + minimum);
+        
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed");
+        }
     }
 }
