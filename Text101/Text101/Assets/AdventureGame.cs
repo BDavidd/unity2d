405949f104ec692a034fed5194d7a8d28c7b882e
David Barzi <barzi.david@gmail.com>
2019-01-26 16:52:56 +0200

    Completed Section 3, Lecture 25

--- Text101/Text101/Assets/AdventureGame.cs
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    private State currentState;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "This is pretty cool!";
+        currentState = startingState;
+        textComponent.text = currentState.GetStateStory();
     }
 
     // Update is called once per frame
