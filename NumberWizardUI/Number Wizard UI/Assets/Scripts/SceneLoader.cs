07e34983eb06d4bd0b801f4f33d8df8c58f3b911
David Barzi <barzi.david@gmail.com>
2019-01-27 17:05:24 +0200

    Completed Section 4, Lecture 40

--- NumberWizardUI/Number Wizard UI/Assets/Scripts/SceneLoader.cs
+++ NumberWizardUI/Number Wizard UI/Assets/Scripts/SceneLoader.cs
@@ -7,7 +7,11 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadNextScene()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(++currentSceneIndex);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex > (SceneManager.sceneCountInBuildSettings - 1))
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
