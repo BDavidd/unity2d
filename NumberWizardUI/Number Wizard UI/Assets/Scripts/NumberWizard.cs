fa8b5e8f29825490e0682b4c64e9f74bc345436f
David Barzi <barzi.david@gmail.com>
2019-01-28 16:37:18 +0200

    Completed Section 4, Lecture 42

---
+++ NumberWizardUI/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int maximum;
+    [SerializeField] int minimum;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        maximum = maximum + 1;
+        GuessAgain();
+    }
+
+    public void OnPressHigher()
+    {
+        minimum = guess;
+        GuessAgain();
+    }
+
+    public void OnPressLower()
+    {
+        maximum = guess;
+        GuessAgain();
+    }
+
+    void GuessAgain()
+    {
+        guess = (maximum + minimum) / 2;
+    }
+}
