302c26ad48e2f448dcddd969e5a397c24a9cfefe
David Barzi <barzi.david@gmail.com>
2019-01-29 13:59:49 +0200

    Completed Section 4, Lecture 44

--- NumberWizardUI/Number Wizard UI/Assets/Scripts/SceneLoader.cs
+++ NumberWizardUI/Number Wizard UI/Assets/Scripts/SceneLoader.cs
@@ -14,4 +14,9 @@ public class SceneLoader : MonoBehaviour
         }
         SceneManager.LoadScene(nextSceneIndex);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
