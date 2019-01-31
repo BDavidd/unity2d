a1e028526498ac44f9f215104a1fae60bc619f1c
David Barzi <barzi.david@gmail.com>
2019-01-31 16:40:52 +0200

    Completed Section 5, Lecture 54

--- Block_Breaker/Block Breaker/Assets/Scripts/Paddle.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Paddle.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     [SerializeField] private float screenWidthInUnits = 16f;
+    [SerializeField] private float paddleSizeInUnits = 2f;
+    
+    private float minX;
+    private float maxX;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        minX = paddleSizeInUnits / 2;
+        maxX = screenWidthInUnits - minX;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float newPositionX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        if (newPositionX < 1)
-        {
-            newPositionX = 1;
-        }
-        else if (newPositionX > screenWidthInUnits - 1)
-        {
-            newPositionX = 15;
-        }
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
 
-        Vector2 paddlePosition = new Vector2(newPositionX, transform.position.y);
+
+        float newPositionX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        paddlePosition.x = Mathf.Clamp(newPositionX, minX, maxX);
         transform.position = paddlePosition;
     }
 }
