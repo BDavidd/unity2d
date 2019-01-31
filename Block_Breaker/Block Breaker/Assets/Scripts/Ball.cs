c579b3168e96e5c5625e9be622132dc3f8e8e247
David Barzi <barzi.david@gmail.com>
2019-01-31 17:49:25 +0200

    Completed Section 5, Lecture 56

--- Block_Breaker/Block Breaker/Assets/Scripts/Ball.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Ball.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Paddle paddle1;
+    [SerializeField] private float xPush = 2f;
+    [SerializeField] private float yPush = 15f;
 
     private Vector2 paddleToBallVector;
-    
+    private Rigidbody2D rigidBodyComp;
+    private bool hasStarted = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidBodyComp = GetComponent<Rigidbody2D>();
         paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
+
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rigidBodyComp.velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector;
