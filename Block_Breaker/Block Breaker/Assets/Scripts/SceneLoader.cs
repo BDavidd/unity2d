c21efe56c67a67fac33401895d01b537630a3a0a
David Barzi <barzi.david@gmail.com>
2019-01-31 11:16:30 +0200

    Completed Section 4, Lecture 48

---
+++ Block_Breaker/Block Breaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex > (SceneManager.sceneCountInBuildSettings - 1))
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
