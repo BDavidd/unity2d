90e613a8ff63fc28b0e9e08dcbc11a35657adc4c
David Barzi <barzi.david@gmail.com>
2019-02-01 19:14:19 +0200

    Completed Section 5, Lecture 67

---
+++ Block_Breaker/Block Breaker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f, 10f)] [SerializeField] private float gameSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
