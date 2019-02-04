45d70dde9f177b7e9664946008968ae47fdb2576
David Barzi <barzi.david@gmail.com>
2019-02-04 18:54:12 +0200

    Completed Section 5, Lecture 69

--- Block_Breaker/Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/GameStatus.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)] [SerializeField] private float gameSpeed = 1f;
     [SerializeField] private int pointsPerBlockDestroyed = 39;
+    [SerializeField] private TextMeshProUGUI scoreText;
     
     [SerializeField] private int currentScore = 0;
 
+    void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+    
+    
     // Update is called once per frame
     void Update()
     {
@@ -18,5 +26,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
