02a055992d8cb06d0f8529d992c03debc2792bea
David Barzi <barzi.david@gmail.com>
2019-02-10 19:24:33 +0200

    Completed Section 6, Lecture 95

--- Laser Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -1,22 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] private GameObject path;
     [SerializeField] private List<Transform> waypoints;
+    [SerializeField] private float moveSpeed = 2f;
+
+    private int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         AddPaths();
+        transform.position = waypoints[waypointIndex].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                ++waypointIndex;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Workaround needed because linking multiple prefabs is no longer possible
