02ac5cb8061b043f6bcdc6472985d7be6ce9dff3
David Barzi <barzi.david@gmail.com>
2019-02-10 16:22:58 +0200

    Completed Section 6, Lecture 88

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float padding = 0.5f;
+
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        var minLimits = new Vector3(0, 0);
+        var maxLimits = new Vector3(1, 1);
+
+        xMin = gameCamera.ViewportToWorldPoint(minLimits).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(maxLimits).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(minLimits).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(maxLimits).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
