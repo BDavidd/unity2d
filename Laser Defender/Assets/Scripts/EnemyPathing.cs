b3f27a0d43e077ea4eef479e1877360c67f218b0
David Barzi <barzi.david@gmail.com>
2019-02-10 19:08:59 +0200

    Completed Section 6, Lecture 95

---
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] private GameObject path;
+    [SerializeField] private List<Transform> waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddPaths();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Workaround needed because linking multiple prefabs is no longer possible
+    private void AddPaths()
+    {
+        waypoints.AddRange(path.transform.GetComponentsInChildren<Transform>());
+
+        // GetComponentsInChildren returns the parent at index 0
+        waypoints.RemoveAt(0);
+    }
+}
