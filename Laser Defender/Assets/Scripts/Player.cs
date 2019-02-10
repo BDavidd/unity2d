0f5f50234af69d9299638b88fb1c4c369813de3d
David Barzi <barzi.david@gmail.com>
2019-02-10 16:55:14 +0200

    Completed Section 6, Lecture 90

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float padding = 0.5f;
+    [SerializeField] private GameObject laserPrefab;
+    [SerializeField] private float projectileSpeed = 20f;
 
     private float xMin;
     private float xMax;
@@ -14,6 +17,33 @@ public class Player : MonoBehaviour
     void Start()
     {
         SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        }
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
     }
 
     private void SetUpMoveBoundaries()
@@ -27,22 +57,5 @@ public class Player : MonoBehaviour
 
         yMin = gameCamera.ViewportToWorldPoint(minLimits).y + padding;
         yMax = gameCamera.ViewportToWorldPoint(maxLimits).y - padding;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
-
-    private void Move()
-    {
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
-
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
-
-        transform.position = new Vector2(newXPos, newYPos);
     }
 }
